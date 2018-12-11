815c3ed546df6a61c4392ad6fefa6858da0693d7
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-11 14:30:07 +0100

    Connects user to performance data in the controller, and modifies the test based on that, test is green

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,8 +1,19 @@
 class Api::V1::PerformanceDataController < ApplicationController
+
+    before_action :authenticate_api_v1_user!
+
     def create
-        @data = PerformanceData.new(params[:perfomance_data])
+        @data = PerformanceData.new(perfomance_data_params.merge(user: current_api_v1_user))
         if @data.save
             render json: { message: 'All good!' }
+        else
+            render json: { error: @data.errors.full_messages }
         end
+    end
+
+    private
+
+    def perfomance_data_params
+        params.require(:perfomance_data).permit!
     end
 end
--- spec/requests/api/v1/perfomance_data_spec.rb
+++ spec/requests/api/v1/perfomance_data_spec.rb
@@ -1,12 +1,15 @@
 RSpec.describe Api::V1::PerformanceDataController, type: :request do
-    let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+    let(:user) { FactoryBot.create(:user) }
+    let(:credentials) { user.create_new_auth_token }    
+    let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
 
     describe 'POST /api/v1/perfomance_data' do
         it 'creates a data entry' do
             post '/api/v1/performance_data', params:
-            { perfomance_data: { data: { message: 'Average' } } }, headers: headers
+            { perfomance_data: { data: { message: 'Average' } } },
+            headers: headers
 
-            entry = PerfomanceData.last
+            entry = PerformanceData.last
             expect(entry.data).to eq 'message' => 'Average'
         end
     end
