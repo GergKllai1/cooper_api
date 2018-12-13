731a97fe0ed806dec0df108171b3eae76af57349
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-13 14:18:04 +0100

    Heroku api error fix trial, changing up the route paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1, defaults: { format: :json } do
+  namespace :v1 do
+    namespace :api, defaults: { format: :json } do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
       resources :performance_data, only: [:create]
       resources :performance_data, only: [:create, :index]
